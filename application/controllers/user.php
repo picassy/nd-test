283d4569f305fe494e701fd9c3b9857b8c12b6e3
Nattaya Dilokwanich <contact.nattaya@gmail.com>
2014-06-19 23:51:49 +0700

    edited login function

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -10,14 +10,7 @@
 			$this->load->model('user/user_model','user_model');
 		}
 
-		public function back_to_register(){
-			$this->load->view('header');
-			$this->load->view('user/registration_view');
-			$this->load->view('footer');
-		}
-
 		public function register(){
-			$this->load->view('header');
 			$data['email'] = $this->input->post('email');
 			$data['password'] = $this->input->post('password');
 			if($data['email']!="0"||$data['password']!="0"){
@@ -25,38 +18,35 @@
 				$data = $this->db->insert('users', $data); 
 				$this->registration_success();
 			} else {
-				$this->load->view('user/registration_view');
+				$this->back_to_register();
 			}
 		}
-
-		/*if($data['email']!="0"||$data['password']!="0"){
-				$data['password'] = md5('\''.$data['password'].'\'');
-				$data = $this->db->insert('users', $data); 
-			}
-			$this->load->view('user/registration_view', $data);
-			$this->load->view('footer');*/
 
 		public function registration_success(){
 			$this->load->view('header');
 			$this->load->view('user/registration_success_view');
+			$this->login();
 			$this->load->view('footer');
 		}
 
-		public function login(){
-			$this->load->view('user/login_form_view');
-			if($this->user_model->isLogin()){
+		public function back_to_register(){
+			$this->load->view('header');
+			$this->load->view('user/registration_view');
+			$this->load->view('footer');
+		}
 
-			}else{
+
+		public function login(){
+			$data['email'] = $this->input->post('email');
+			$data['password'] = md5('\''.$this->input->post('password').'\'');
+			if ($this->user_model->login($data)) {
+				header('Location: ' . site_url('home/'));
+			} else {
+				echo "login failed";
 				$this->load->view('user/login_form_view');
-				$data['email'] = $this->input->post('email');
-				$data['password'] = md5('\''.$this->input->post('password').'\'');
-				if($this->user_model->login($data)){
-					header('Location: '.site_url(''));
-				}else{
-					echo "login failed";
-				}
+				//echo '<p><a href="login">กลับหน้าแรก</a></p>';
+				//echo '<p><a href="register">หรือ สมัครสมาชิก</a></p>';
 			}
-			
 		}
 
 	}
