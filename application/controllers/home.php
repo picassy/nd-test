f8ea1445701db5c72a791a7b08a626342308edf7
Nattaya Dilokwanich <contact.nattaya@gmail.com>
2014-06-22 23:56:58 +0700

    added function logout

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,25 +4,27 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->load->helper('cookie');
 		$this->load->helper('url'); //for base_url using
+		$this->load->library('session');
 		//load model
 		$this->load->model("user/user_model", "user_model");
 	}
 
 	public function index() {
-
-		$this->load->view('header');
-		//if haven't logged yet, show login_form_view
-		$this->load->view('home_view');
-		$this->load->view('footer');
 		if ($this->user_model->isLogin()) {
 			$this->load->view('header');
+			//will change this view to something else later.
+			$this->load->view('home_view');
 			$this->load->view('footer');
 		} else {
-			header('Location: '.site_url(''));
-			echo "dddd";
+			//something.
 		}
+	}
+	
+	public function logout(){
+		$this->session->unset_userdata($user_id);
+		$this->session->sess_destroy();
+		redirect(base_url('index.php/login'), 'refresh');
 	}
 }
 ?>
